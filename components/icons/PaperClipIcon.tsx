bebaed0d7b370d3c00d313436f89bc94b9853f2b
Ismael Farah <167617802+IsmaelFarah88@users.noreply.github.com>
2025-08-10 02:26:43 +0330

    Add files via upload

---
+++ components/icons/PaperClipIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const PaperClipIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    fill="none" 
+    viewBox="0 0 24 24" 
+    strokeWidth={1.5} 
+    stroke="currentColor" 
+    {...props}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1118.375 9.09l-7.693 7.693a1.5 1.5 0 01-2.121-2.121l5.303-5.303" />
+  </svg>
+);
